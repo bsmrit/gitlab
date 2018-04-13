db44d83f679eaa7bdb91dcf05eb691dcf3a8fc80
Benjamin Stevens <benjaminastevens@gmail.com>
2018-04-12 20:55:14 -0700

    Added code in hello.php to make it HTML5 valid.

--- hello.php
+++ hello.php
@@ -1,2 +1,11 @@
-<?php
-    echo 'Hello, world!';
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP Hello</title>
+    </head>
+    <body>
+    <?php
+        echo '<p>Hello, world!</p>';
+    ?>
+    </body>
+</html>
